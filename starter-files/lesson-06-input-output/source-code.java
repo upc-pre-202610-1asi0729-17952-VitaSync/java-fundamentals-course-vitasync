b55d53c51b988bedeca14f19efb784569515f6bf
Leonardo Rafael Garcia Villanueva <leonardo_351461@hotmail.com>
2026-06-12 01:44:51 -0500

    feat: added lesson-06 starter file

--- starter-files/lesson-06-input-output/source-code.java
+++ starter-files/lesson-06-input-output/source-code.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
   
     public static void main(String[] args) {
-      
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Cual es tu nombre?");
+        String nombre = entrada.nextLine();
+
+        // TODO: Pedir la edad y almacenarla
+
+        // TODO: Mostrar un mensaje usando el nombre y la edad
+
+        entrada.close();
     }
   
 }
