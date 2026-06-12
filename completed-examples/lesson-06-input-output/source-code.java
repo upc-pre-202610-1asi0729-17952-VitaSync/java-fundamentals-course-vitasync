c8d6eee0177df328391d9acc15460d095902d971
Leonardo Rafael Garcia Villanueva <leonardo_351461@hotmail.com>
2026-06-12 02:07:06 -0500

    feat: added lesson-06 example

--- completed-examples/lesson-06-input-output/source-code.java
+++ completed-examples/lesson-06-input-output/source-code.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
   
     public static void main(String[] args) {
-      
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Cual es tu nombre?");
+        String nombre = entrada.nextLine();
+
+        System.out.println("Que edad tienes?");
+        int edad = entrada.nextInt();
+
+        entrada.nextLine();
+
+        System.out.println("Cual es tu curso favorito?");
+        String cursoFavorito = entrada.nextLine();
+
+        mostrarResumen(nombre, edad, cursoFavorito);
+
+        entrada.close();
+    }
+
+    public static void mostrarResumen(String nombre, int edad, String cursoFavorito) {
+        System.out.println("Resumen del estudiante:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Curso favorito: " + cursoFavorito);
     }
   
 }
