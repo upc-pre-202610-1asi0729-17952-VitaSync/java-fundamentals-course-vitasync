1aa9497b24cf15392857ca9f5a4a02b3be9faf66
Leonardo Rafael Garcia Villanueva <leonardo_351461@hotmail.com>
2026-06-12 01:16:29 -0500

    feat: added lesson-05 starter file

--- starter-files/lesson-05-functions-and-parameters/source-code.java
+++ starter-files/lesson-05-functions-and-parameters/source-code.java
@@ -1,7 +1,32 @@
 public class Main {
-  
     public static void main(String[] args) {
       
+        double area = CalcularAreaRectangulo(5, 3);
+        System.out.println("El area del rectángulo es: " + area);
+
+        // TODO: Calcula el area de una caja con las dimensiones de largo ancho y alto con los valores 4, 2 y 3
+
+        String mensaje = CrearMensajeBienvenida("tu nombre");
+        System.out.println(mensaje);
+
+        // TODO: Crea un mensaje de bienvenida con un curso en especifico
+    }
+
+    public static double CalcularAreaRectangulo(double base, double altura) {
+        return base * altura;
+    }
+
+    public static double CalcularVolumenCubo(double largo, double ancho, double alto) {
+        // TODO: devolver el volumen calculado
+        return 0;
+    }
+
+    public static String CrearMensajeBienvenida(String nombre) {
+        return "Hola, " + nombre + ". Bienvenido al curso.";
+    }
+
+    public static String CrearMensajeBienvenida(String nombre, String curso) {
+        return "Hola, " + nombre + ". Bienvenido al curso de " + curso + ".";
     }
   
 }
