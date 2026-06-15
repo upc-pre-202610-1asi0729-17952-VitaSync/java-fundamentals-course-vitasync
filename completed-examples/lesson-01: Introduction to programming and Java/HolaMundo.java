bf551800198235771dfcab0c7579015d4783217b
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-06-14 19:42:22 -0500

    feat: fix full code for lesson 1

--- completed-examples/lesson-01: Introduction to programming and Java/HolaMundo.java
+++ completed-examples/lesson-01: Introduction to programming and Java/HolaMundo.java
@@ -1,1 +1,7 @@
-
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Bienvenido a Java!");
+        System.out.println("La programación es como darle instrucciones a una computadora.");
+    }
+}
