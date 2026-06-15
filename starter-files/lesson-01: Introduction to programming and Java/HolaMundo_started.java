642bdf652399246014083fb2d6e1faf914071b67
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-06-14 19:44:30 -0500

    feat: fix started file for lesson 1

--- starter-files/lesson-01: Introduction to programming and Java/HolaMundo_started.java
+++ starter-files/lesson-01: Introduction to programming and Java/HolaMundo_started.java
@@ -1,1 +1,16 @@
+public class HolaMundo {
+    public static void main(String[] args) {
 
+        // TODO 1: Usa System.out.println() para imprimir el mensaje "¡Hola, Mundo!"
+        // Escribe tu instrucción aquí:
+
+
+        // TODO 2: Imprime un segundo mensaje de bienvenida con el texto que quieras
+        // Ejemplo: System.out.println("¡Bienvenido a Java!");
+
+
+        // TODO 3: Imprime una tercera línea diciéndole algo a la computadora
+        // ¡Sé creativo!
+
+    }
+}
