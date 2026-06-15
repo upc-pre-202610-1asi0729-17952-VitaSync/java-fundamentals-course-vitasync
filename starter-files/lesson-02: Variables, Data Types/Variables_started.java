8e9b5797e1796d52ea64bba281fed1b3bb7b1875
Fernando Julio Güere Calero <fguerec17@gmail.com>
2026-06-14 19:56:15 -0500

    feat: fix started file for lesson 2

--- starter-files/lesson-02: Variables, Data Types/Variables_started.java
+++ starter-files/lesson-02: Variables, Data Types/Variables_started.java
@@ -1,1 +1,31 @@
+public class Variables {
+    public static void main(String[] args) {
 
+        // TODO 1: Declara una variable de tipo int con tu edad
+        // Formato: int nombreVariable = valor;
+        // Escribe aquí:
+
+
+        // TODO 2: Declara una variable de tipo String con tu nombre
+        // Recuerda: el texto va entre comillas dobles
+        // Escribe aquí:
+
+
+        // TODO 3: Declara una variable de tipo double con tu altura en metros
+        // Usa punto decimal, no coma. Ejemplo: 1.70
+        // Escribe aquí:
+
+
+        // TODO 4: Declara una variable de tipo boolean
+        // Solo puede tener el valor true o false
+        // Escribe aquí:
+
+
+        // TODO 5: Muestra todas las variables en pantalla con System.out.println()
+        // Puedes unir texto y variables con el símbolo +
+        // Ejemplo: System.out.println("Mi edad es: " + edad);
+        // Escribe aquí:
+
+
+    }
+}
